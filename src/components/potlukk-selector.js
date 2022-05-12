0e3072a468b1375c63303296a480f3d927c5ea90
justfitch <81538565+justfitch@users.noreply.github.com>
2022-05-12 13:21:53 -0600

    Try again

---
+++ src/components/potlukk-selector.js
@@ -0,0 +1,10 @@
+
+export default function PotlukkSelector(){
+
+    return(<>
+    
+    <h1>Pick A Potlukk:</h1>
+    
+    </>)
+
+}
---
+++ src/components/potlukk-viewer.js
@@ -0,0 +1,15 @@
+
+export default function PotlukkViewer(){
+
+    return(<>
+    <table>
+        <thead>
+            <tr><th>Potlukk ID</th><th>Date/Time</th><th>Host</th></tr>
+        </thead>
+        <tbody>
+            <td>1</td><td>1652901045000</td><td>John</td>
+        </tbody>
+    </table>
+    
+    </>)
+}
