c5f703488e4fcd96d0acd9b6af29b22e1347a229
justfitch <81538565+justfitch@users.noreply.github.com>
2022-05-12 13:20:24 -0600

    Initial Outline Project

---
+++ src/components/SplashPage.js
@@ -0,0 +1,12 @@
+
+
+export default function SplashPage(){
+
+    return(<>
+    <h1>Welcome to Potlukk!</h1>
+    <h3>Chose an Option:</h3>
+    <button>Select an Existing Potlukk</button>
+    <button>Register a New User</button>
+    <button>View a Private Potlukk</button>
+    </>)
+}
