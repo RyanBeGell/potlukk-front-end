3e31fd714088927710b8fcbd4aceebbed67427e5
Jonathan Langston <jtlangston96@gmail.com>
2022-05-15 16:12:52 -0400

    Can now add items through localhost

--- src/components/add-items.js
+++ src/components/add-items.js
@@ -1,5 +1,68 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
 
 export default function AddItems(){
+
+    const navigate = useNavigate();
+
+    const [items, setItems] = useState([]);
+
+    async function getItemsByPotluck(){
+
+        const response = await fetch("http://localhost:8080/potlucks/7/items");
+        const body = await response.json();
+        setItems(body);
+
+    }
+
+    useEffect(()=>{
+        getItemsByPotluck();
+    },[]);
+
+    const itemsMissingRows = items.filter(i => i.status !== "Fulfilled")
+    .map(i => <tr key ={i.item_id}>
+        <td>{i.description}</td>
+        <td>{i.status}</td>
+        <td><button>Delete</button></td>
+    </tr>);
+
+    const itemsFulfilledRows = items.filter(i => i.status === "Fulfilled")
+    .map(i => <tr key ={i.item_id}>
+        <td>{i.description}</td>
+        <td>{i.status}</td>
+        <td>{i.supplier}</td>
+        <td><button>Delete</button></td>
+    </tr>);
+
+    const [description, setDescription] = useState("");
+    const [status, setStatus] = useState("");
+
+    function updateDescription(event){
+        setDescription(event.target.value);
+    }
+
+    function updateStatus(event){
+        setStatus(event.target.value);
+    }
+
+    async function createItem(){
+        const item = {description:description, status:status, supplier:"", potluckId:7};
+
+        const response = await fetch("http://localhost:8080/items",{
+            body:JSON.stringify(item),
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            }
+        });
+
+        if(response.status === 201){
+            getItemsByPotluck();
+        }
+        else{
+            alert("Failed to add item.");
+        }
+    }
 
     return(<>
     
@@ -11,17 +74,17 @@ export default function AddItems(){
             <tbody>
                 <tr>
                     <td>
-                    <input name ="item"/>
+                    <input onChange={updateDescription} name ="item"/>
                     </td>
                     <td>
-                        <input name="status" list="Status"/>
+                        <input onChange={updateStatus} name="status" list="Status"/>
                             <datalist id = "Status">
                                 <option value="Wanted"></option>
                                 <option value="Needed"></option> 
                             </datalist>
                     </td>
                     <td>
-                        <button>Add Item</button> 
+                        <button onClick={createItem} >Add Item</button> 
                     </td>    
                 </tr>    
             </tbody>
@@ -35,8 +98,7 @@ export default function AddItems(){
             <tr><th>Item</th><th>Status</th></tr>
         </thead>
         <tbody>
-            <tr><td>Potatos</td><td>Needed</td></tr>
-            <tr><td>Cake</td><td>Wanted</td></tr>
+            {itemsMissingRows}
         </tbody>
     </table>
 <br/><h3>Fulfilled Items</h3>
@@ -45,12 +107,12 @@ export default function AddItems(){
             <tr><th>Item</th><th>Status</th><th>Supplier</th></tr>
         </thead>
         <tbody>
-            <tr><td>Burgers</td><td>Fulfilled</td><td>John</td></tr>
+            {itemsFulfilledRows}
         </tbody>
     </table>
     
     <br/>
-    <button>Return to Home</button>
+    <button onClick={()=>navigate("/")}>Return to Home</button>
     
     </>)
 
